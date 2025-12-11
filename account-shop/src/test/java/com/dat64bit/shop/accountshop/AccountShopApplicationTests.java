f50782290689748b3058c4494186a3fa83b436a1
dat64-bit <vdat2870@gmail.com>
2025-12-11 21:43:15 +0700

    Up Project Template

---
+++ account-shop/src/test/java/com/dat64bit/shop/accountshop/AccountShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dat64bit.shop.accountshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
